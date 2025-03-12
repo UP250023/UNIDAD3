d49398e49d9fedbbd57d6051c7deea03f91451c4
UP250023 <up250023@alumnos.upa.edu.mx>
2025-03-12 15:16:14 -0600

    Add files via upload

---
+++ PYTHON/#04_DayExcercise1-25.py
@@ -0,0 +1,131 @@
+#04_DayExcercise1
+
+word = 'Treinta'
+word1 = 'Diaz'
+word2 = 'De'
+word3 = 'Python'
+space = ' '
+
+print (word + space + word1 + space + word2 +space + word3)
+
+#04_DayExcercise2
+
+word = 'Coding'
+word1 = 'For'
+word2 = 'all'
+space = ' '
+print (word + space + word1 +  space + word2)
+
+#04_DayExcercise3
+
+company = ('Coding\tFor\tAll')
+
+##04_DayExcercise4
+
+print(company)
+
+#04_DayExcercise5 
+
+letter = company
+print(letter)
+print((len(company)))
+
+#04_DayExcercise6
+
+print(company.upper())
+
+#04_DayExcercise7
+
+print(company.lower())
+
+
+#04_DayExcercise8
+
+
+print(company.capitalize()) 
+
+
+print(company.title())  
+
+
+print(company.swapcase())
+
+#04_DayExcercise9
+
+print(company[0:6])
+
+#04_DayExcercise10
+
+print(company.find('Coding'))
+
+#04_DayExcercise11
+
+print(company.replace('Coding','python'))
+
+#04_DayExcercise12
+
+print('Python everyone'.replace('everyone','python'))
+
+#04_DayExcercise13
+
+print(company.split())
+
+#04_DayExcercise14
+
+print('Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'.split(','))
+
+#04_DayExcercise15
+
+print(company[0])
+
+#04_DayExcercise16
+
+print(company[-1])
+
+#04_DayExcercise17
+
+print(company[10])
+
+#04_DayExcercise18
+
+print('Python For Everyone'.replace('Python For Everyone', 'PFE'))
+
+#04_DayExcercise19
+
+print('Coding For All'.replace('Coding For All', 'CFA'))
+
+#04_DayExcercise20
+
+print(company.index('C'))
+
+#04_DayExcercise21
+
+print(company.index('F'))
+
+#04_DayExcercise22
+
+print('Coding For All People'.rfind('l'))
+
+#04_DayExcercise23
+
+print("You cannot end a sentence with because because because is a conjunction".index('because'))
+
+#04_DayExcercise24
+
+print("You cannot end a sentence with because because because is a conjunction".rindex('because'))
+
+#04_DayExcercise25
+
+print("You cannot end a sentence with because because because is a conjunction"[31:54])
+
+
+
+
+
+
+
+
+
+
+
+
